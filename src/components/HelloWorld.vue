568ab79f8beb4569ed0748e0bf00fb1d7fa74112
jhChoi <jihye3636@nate.com>
2018-11-29 16:33:07 +0900

    test 입니다.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -92,9 +92,9 @@ export default {
     }
   }
 }
-</script> 
+</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only --> 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
   font-weight: normal;
